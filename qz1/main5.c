c6040d8499a55cfb5f56e51081c17e5916a08809
B1129001 <119854519+B1129001@users.noreply.github.com>
2025-02-27 13:59:38 +0800

    Update main5.c

--- qz1/main5.c
+++ qz1/main5.c
@@ -3,15 +3,12 @@
 int main() {
     int n = 7;
     int count = 0;
-
     while (n) {
         if (n & 1) {
             count++;
         }
         n >>= 1;
     }
-
     printf("%d\n", count);
-
     return 0;
 }
